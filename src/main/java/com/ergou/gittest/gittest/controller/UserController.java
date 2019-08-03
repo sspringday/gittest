5242e5d20fc0564e865021945765bb39d7ab9746
sspringday <1838941319@qq.com>
2019-08-03 11:32:18 +0800

    git test

---
+++ src/main/java/com/ergou/gittest/gittest/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.ergou.gittest.gittest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 程二狗
+ * @since 2019/8/3 0003 11:24
+ */
+
+@RestController
+public class UserController {
+    public String sayGit(){
+        return  "hello git";
+    }
+}
